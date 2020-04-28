5ad9e86dfa3f80b529472b83d7b3e0bf95d4acc2
mrvollger <mrvollger@gmail.com>
2020-04-28 05:00:46 -0700

    add

---
+++ scripts/split_fasta.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import argparse
+import os 
+import sys
+import pysam
+# global var for inputs
+args=None 
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument("infile", help="positional input")
+	parser.add_argument("-n", help="length of each window", type=int, default=5*10**6)
+	parser.add_argument('-d', help="store args.d as true if -d",  action="store_true", default=False)
+	args = parser.parse_args()
+	
+	fasta = pysam.FastaFile(args.infile)
+
+	for name, length in zip(fasta.references, fasta.lengths):
+		seq = fasta.fetch(name)
+		
+		for start in range(0, length, args.n):
+			end = min(length, start+args.n)
+			sys.stdout.write(">{}:{}-{}\n{}\n".format(name, start+1, end, seq[start:end] ) )
+
+
+
+
